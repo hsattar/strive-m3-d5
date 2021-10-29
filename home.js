e2e5dec7c3f826a9b7db6ee341f47e62d2c80852
hsattar <39243545+hsattar@users.noreply.github.com>
2021-10-29 11:46:54 +0100

    displaying all artists on home page with api

--- home.js
+++ home.js
@@ -1,12 +1,11 @@
-const artist1 = document.querySelector('#artist1')
-const artist2 = document.querySelector('#artist2')
-const artist3 = document.querySelector('#artist3')
-const artist4 = document.querySelector('#artist4')
-
-const album1 = document.querySelector('#album1')
-const album2 = document.querySelector('#album2')
-const album3 = document.querySelector('#album3')
-const album4 = document.querySelector('#album4')
+// const artist1 = document.querySelector('#artist1')
+// const artist2 = document.querySelector('#artist2')
+// const artist3 = document.querySelector('#artist3')
+// const artist4 = document.querySelector('#artist4')
+// const album1 = document.querySelector('#album1')
+// const album2 = document.querySelector('#album2')
+// const album3 = document.querySelector('#album3')
+// const album4 = document.querySelector('#album4')
 
 const artistContainer1 = document.querySelector('#artistContainer1')
 const artistContainer2 = document.querySelector('#artistContainer2')
@@ -18,11 +17,6 @@ const albumContainer2 = document.querySelector('#albumContainer2')
 const albumContainer3 = document.querySelector('#albumContainer3')
 const albumContainer4 = document.querySelector('#albumContainer4')
 
-console.log(artist1)
-console.log(album2)
-console.log(artistContainer1)
-console.log(albumContainer1)
-
 const fetchData = (query) => {
     return fetch(`https://striveschool-api.herokuapp.com/api/deezer/artist/${query}`)
     .then(response => response.json())
@@ -31,7 +25,7 @@ const fetchData = (query) => {
 
 const displayData = (container, artist) => {
     fetchData(artist)
-    .then(artist => container.innerHTML =  `
+    .then(artist => container.innerHTML +=  `
     <div class="card hp-subhero-card col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2">
         <img src="${artist.picture_xl}" class="card-img-top pt-2 img-fluid" alt="...">
         <div class="card-body">
@@ -41,8 +35,31 @@ const displayData = (container, artist) => {
     )
 }
 
-const artists1 = ['ariana', 'eminem', 'taylor', 'rhianna', 'queen', 'queen']
+const artists = [
+    ['1562681', '13', '12246', '246791', '564'],
+    ['12178', '5306539', '4104927', '1188', '384236'],
+    ['9507694', '9236850', '4794268', '8706544', '12088868'],
+    ['293585', '75798', '11152580', '429675', '145']
+]
 
-artists1.forEach(artist => {
+artists[0].forEach(artist => {
     displayData(artistContainer1, artist)
 })
+
+artists[1].forEach(artist => {
+    displayData(artistContainer2, artist)
+})
+
+artists[2].forEach(artist => {
+    displayData(artistContainer3, artist)
+})
+
+artists[3].forEach(artist => {
+    displayData(artistContainer4, artist)
+})
+
+// for (let i = 0; i < artists.length; i++) {
+//     artists[i].forEach(artist => {
+//         displayData(`artistContainer${i}`, artist)
+//     })
+// }
