5bee1d1e2c7e4bf3973449c404b0c34e55782cf0
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-01 21:21:53 +0000

    displaying album data

--- albums/albums.js
+++ albums/albums.js
@@ -25,3 +25,42 @@ songInfo.forEach(song => {
 })
 
 const albumSongsList = document.querySelector('#albumSongsList')
+
+const fetchAlbum = query => {
+    fetch(`https://striveschool-api.herokuapp.com/api/deezer/album/${query}`)
+    .then(response => response.json())
+    .then(album => displayAlbum(album))
+    .catch(error => console.error(error))
+}
+
+const displayAlbum = album => {
+    console.log(album)
+    const albumCover = document.querySelector('.album-cover')
+    const albumTitle = document.querySelector('.album-title')
+    const albumArtist = document.querySelector('.album-artist')
+    const albumYear = document.querySelector('.album-year')
+    const albumLength = document.querySelector('.album-length')
+    const albumDuration = document.querySelector('.album-duration')
+    const albumSongsList = document.querySelector('#albumSongsList')
+    albumCover.src = `${album.cover_xl}`
+    albumTitle.innerText = `${album.title}`
+    albumArtist.innerText = `${album.artist.name}`
+    albumYear.innerText = `${album.release_date}`
+    albumLength.innerText = `${album.nb_tracks} songs`
+    albumDuration.innerText = `${album.duration}`
+    albumSongsList.innerHTML = album.tracks.data.map(album => `
+    <div class="row song-info my-2">
+
+        <div class="col-11">
+            <p class="my-0 font-weight-bold">${album.title}</p>
+            <p class="mt-0 mb-2 smaller-text light-gray-text">${album.artist.name}</p>
+        </div>
+
+        <div class="col-1 light-gray-text">
+            <p>${album.duration}</p>
+        </div>
+    
+    </div>`).join('')
+}
+
+fetchAlbum(15478674)
