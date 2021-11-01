f6f53fa755050c4adbdd0e92ce8360e8f697b558
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-01 19:46:37 +0000

    removed redundant code

--- artists/artist.js
+++ artists/artist.js
@@ -11,10 +11,13 @@ const fetchData = query => {
 const displayArtistData = (data) => {
     const artistName = document.querySelector('.artist-name')
     const albumHeader = document.querySelector('.album-header-image')
+    const postedByArtist = document.querySelector('#postedByArtist')
+    const artistImg = document.querySelector('#artistImg')
     artistName.innerText = data.name
     albumHeader.style.backgroundImage = `url(${data.picture_xl})`
 } 
 
 
 const albumId = new URLSearchParams(window.location.search).get('album')
-fetchData(albumId)
+if (albumId !== null) fetchData(albumId)
+if (albumId === null) fetchData(12246)
