8e0924ed2c1f4e8de715d284074bea5cb2817ae2
hsattar <39243545+hsattar@users.noreply.github.com>
2021-11-01 20:34:08 +0000

    Displaying Artist songs dynamically

--- artists/artist.js
+++ artists/artist.js
@@ -1,21 +1,42 @@
 const fetchData = query => {
     fetch(`https://striveschool-api.herokuapp.com/api/deezer/artist/${query}`)
         .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            displayArtistData(data)
-        })
+        .then(data => displayArtistData(data))
         .catch(error => console.error(error))
 }
             
-const displayArtistData = (data) => {
+const displayArtistData = data => {
     const artistName = document.querySelector('.artist-name')
     const albumHeader = document.querySelector('.album-header-image')
-    const postedByArtist = document.querySelector('#postedByArtist')
-    const artistImg = document.querySelector('#artistImg')
     artistName.innerText = data.name
     albumHeader.style.backgroundImage = `url(${data.picture_xl})`
+    fetchArtistSongs(data)
 } 
+
+const fetchArtistSongs = data => {
+    fetch(`${data.tracklist}`)
+    .then(response => response.json())
+    .then(songs => displayArtistSongs(songs))
+    .catch(error => console.error(error))
+}
+
+const displayArtistSongs = songs => {
+    const popSongsList = document.querySelector('#popSongsList')
+    console.log(songs.data)
+    popSongsList.innerHTML = songs.data.map(song => `
+    <div class="row song-info my-2">
+
+        <div class="col-11">
+            <p class="my-0 font-weight-bold">${song.title}</p>
+            <p class="mt-0 mb-2 smaller-text light-gray-text">${song.album.title}</p>
+        </div>
+
+        <div class="col-1 light-gray-text">
+            <p>${song.duration}</p>
+        </div>
+    
+    </div>`).join('')
+}
 
 
 const albumId = new URLSearchParams(window.location.search).get('album')
